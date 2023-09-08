de50130505e7444f72634085bf6f780ceeeb5e6f
Opeseen <Opeyemi5055@yahoo.com>
2023-09-08 19:50:01 +0100

    Deployed

---
+++ hello-spring/src/test/java/com/opeyemi/springboot/hellospring/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opeyemi.springboot.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
