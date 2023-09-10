efedd851d15683920ae9f087cc1cc15015828f1c
Opeseen <Opeyemi5055@yahoo.com>
2023-09-10 22:14:41 +0100

    Deployed

--- LectureSpring/grade-submission/src/main/java/com/opeyemi/gradesubmission/GradeController.java
+++ LectureSpring/grade-submission/src/main/java/com/opeyemi/gradesubmission/GradeController.java
@@ -1,5 +1,8 @@
 package com.opeyemi.gradesubmission;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,11 +10,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GradeController {
+  List<Grade> studentGrades = Arrays.asList(
+    new Grade("Harry","Maths","C-"),
+    new Grade("Mike","English","A-"),
+    new Grade("John","Econs","B+")
+  );
   
+  @GetMapping("/conditionals")
+  public String getMethodName(Model model){
+    model.addAttribute("sale",150);
+    model.addAttribute("sales",50);
+    model.addAttribute("product", "table");
+    return "conditionals";
+  }
+
   @GetMapping("/grade")
   public String getGrades(Model model){
-    Grade grade = new Grade("Harry","Maths","C-");
-    model.addAttribute("grade",grade);
+    model.addAttribute("grade", studentGrades);
     return "grades";
+  }
+
+  @GetMapping(value = "/")
+  public String getMethodValue(Model model){
+    model.addAttribute("menu", "we sell chocolate rice cakes bubble tea");
+    return "view";
   }
 }
