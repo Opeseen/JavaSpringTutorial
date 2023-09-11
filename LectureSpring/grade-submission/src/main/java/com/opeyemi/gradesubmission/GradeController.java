da2b6b08ae6fee7cb35d4f43e9c9f6f04757fc72
Opeseen <Opeyemi5055@yahoo.com>
2023-09-12 00:56:41 +0100

    Deployed

--- LectureSpring/grade-submission/src/main/java/com/opeyemi/gradesubmission/GradeController.java
+++ LectureSpring/grade-submission/src/main/java/com/opeyemi/gradesubmission/GradeController.java
@@ -1,13 +1,13 @@
 package com.opeyemi.gradesubmission;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -24,7 +24,6 @@ public class GradeController {
 
   @PostMapping("/handleSubmit")
   public String SubmitForm(Grade grade){
-    System.out.println(grade);
     studentGrades.add(grade);
     return "redirect:/grade";
   }
@@ -35,6 +34,13 @@ public class GradeController {
     return "grades";
   }
 
+  public Integer getGradeIndex(String name){
+    for(int i = 0; i < studentGrades.size(); i++){
+      if(studentGrades.get(i).getName().equals(name)) return i;    
+    }
+    return -1000;
+  }
+
   @GetMapping(value = "/view")
   public String getMethodValue(Model model){
     model.addAttribute("menu", "we sell chocolate rice cakes bubble tea");
@@ -42,8 +48,8 @@ public class GradeController {
   }
 
   @GetMapping(value = "/")
-  public String gradeForm(Model model){
-    model.addAttribute("grade", new Grade());
+  public String gradeForm(Model model, @RequestParam(required = false) String name){
+    model.addAttribute("grade", getGradeIndex(name) == -1000 ? new Grade() : studentGrades.get(getGradeIndex(name)));
     return "form";
   }
 
