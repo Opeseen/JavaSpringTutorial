14173be1fb5b93fae8decc6549232f26cf1dafc0
Opeseen <Opeyemi5055@yahoo.com>
2023-09-14 00:02:20 +0100

    Deployed

---
+++ Challenge_Workbook_Spring/MVC/Challenge02/global-superstore/src/main/java/com/opeyemi/globalsuperstore/Constants.java
@@ -0,0 +1,5 @@
+package com.opeyemi.globalsuperstore;
+
+public class Constants {
+  public static final String[] CATEGORIES = new String[] {"Furniture","Office Supplies","Technology"};
+}
---
+++ Challenge_Workbook_Spring/MVC/Challenge02/global-superstore/src/test/java/com/opeyemi/globalsuperstore/GlobalSuperstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opeyemi.globalsuperstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalSuperstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
