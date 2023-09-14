aceac30890cddd0db7f19d5b19b875590e7559eb
Opeseen <Opeyemi5055@yahoo.com>
2023-09-15 00:36:09 +0100

    Deployed

--- Challenge_Workbook_Spring/MVC/Challenge02/global-superstore/src/main/java/com/opeyemi/globalsuperstore/SuperstoreController.java
+++ Challenge_Workbook_Spring/MVC/Challenge02/global-superstore/src/main/java/com/opeyemi/globalsuperstore/SuperstoreController.java
@@ -1,16 +1,34 @@
 package com.opeyemi.globalsuperstore;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SuperstoreController {
+    List<Item> items = new ArrayList<>();
     @GetMapping("/")
     public String getForm(Model model){
         model.addAttribute("categories", Constants.CATEGORIES);
-
+        model.addAttribute("item", new Item());
         return "form";
     }
 
+    @GetMapping("/inventory")
+    public String getInventory(Model model){
+        model.addAttribute("Inventory", items);
+        
+        return "inventory";
+    }
+    
+    @PostMapping("/submitItem")
+    public String submitInventory(Item item){
+        items.add(item);
+        System.out.println(items);
+        return "redirect:/inventory";
+    }
 }
