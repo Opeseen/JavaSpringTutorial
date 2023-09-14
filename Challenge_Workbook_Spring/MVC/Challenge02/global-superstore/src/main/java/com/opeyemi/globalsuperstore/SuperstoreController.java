644f9ced54a8451f9293d4cd49d606ec36c2fcfe
Opeseen <Opeyemi5055@yahoo.com>
2023-09-14 18:57:03 +0100

    Deployed

---
+++ Challenge_Workbook_Spring/MVC/Challenge02/global-superstore/src/main/java/com/opeyemi/globalsuperstore/SuperstoreController.java
@@ -0,0 +1,16 @@
+package com.opeyemi.globalsuperstore;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SuperstoreController {
+    @GetMapping("/")
+    public String getForm(Model model){
+        model.addAttribute("categories", Constants.CATEGORIES);
+
+        return "form";
+    }
+
+}
