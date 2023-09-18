46bed7d7c8b0da8f60ac8b2c70bfd890d5d2f071
Opeseen <Opeyemi5055@yahoo.com>
2023-09-18 20:08:12 +0100

    Deployed

---
+++ Challenge_Workbook_Spring/FIELD_VALIDATION/field-validation-one/src/main/java/com/opeyemi/fieldvalidationone/FieldValidationOneApplication.java
@@ -0,0 +1,13 @@
+package com.opeyemi.fieldvalidationone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldValidationOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FieldValidationOneApplication.class, args);
+	}
+
+}
