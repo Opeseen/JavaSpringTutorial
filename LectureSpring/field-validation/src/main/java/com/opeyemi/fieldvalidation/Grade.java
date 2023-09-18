fce86c596081a3f3b8de99a913aaf3d5e44850e3
Opeseen <Opeyemi5055@yahoo.com>
2023-09-18 22:37:56 +0100

    Deployed

--- LectureSpring/field-validation/src/main/java/com/opeyemi/fieldvalidation/Grade.java
+++ LectureSpring/field-validation/src/main/java/com/opeyemi/fieldvalidation/Grade.java
@@ -10,6 +10,7 @@ public class Grade {
   private String name;
   @NotBlank(message = "Subject field cannot be blank")
   private String subject;
+  @Score(message = "Invalid Score grade")
   private String score;
   private String id;
 
