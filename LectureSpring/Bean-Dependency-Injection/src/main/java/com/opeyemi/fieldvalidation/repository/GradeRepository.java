f8352e7bf3aa4b5212af142d9b9d8d3d71721397
Opeseen <Opeyemi5055@yahoo.com>
2023-09-20 18:54:45 +0100

    Deployed

---
+++ LectureSpring/Bean-Dependency-Injection/src/main/java/com/opeyemi/fieldvalidation/repository/GradeRepository.java
@@ -0,0 +1,29 @@
+package com.opeyemi.fieldvalidation.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.opeyemi.fieldvalidation.Grade;
+
+@Component
+public class GradeRepository {
+   private List<Grade> studentGrades = new ArrayList<>();
+
+   public Grade getGrade(int index){
+    return studentGrades.get(index);
+   }
+
+   public void addGrade(Grade grade){
+    studentGrades.add(grade);
+   }
+
+   public void updateGrade(Grade grade, int index){
+    studentGrades.set(index, grade);
+   }
+
+   public List<Grade> getGrades(){
+    return studentGrades;
+   }
+}
