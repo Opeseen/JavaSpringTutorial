6b171d89ed9f7c5ec0dc7c8d952ae1113f71d74d
Opeseen <Opeyemi5055@yahoo.com>
2023-09-21 11:46:24 +0100

    Deployed

---
+++ Challenge_Workbook_Spring/DEPENDENCY-INJECTION/Challenge-Depend-Injec/src/main/java/com/opeyemi/fieldvalidation/service/GradeService.java
@@ -0,0 +1,57 @@
+package com.opeyemi.fieldvalidation.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.opeyemi.fieldvalidation.Constant;
+import com.opeyemi.fieldvalidation.Grade;
+import com.opeyemi.fieldvalidation.repository.GradeRepository;
+
+@Service
+public class GradeService {
+    @Autowired
+    GradeRepository gradeRepository;
+    
+    public Grade getGrade(int index){
+        return gradeRepository.getGrade(index);
+    }
+
+    public void addGrade(Grade grade){
+        gradeRepository.addGrade(grade);
+    }
+
+    public void updateGrade(Grade grade, int index){
+        gradeRepository.updateGrade(grade,index);
+    }
+
+    public List<Grade> getGrades(){
+        return gradeRepository.getGrades();
+    }
+
+    public int getGradeIndex(String id){
+        for(int i = 0; i < getGrades().size(); i++){
+            if(getGrade(i).getId().equals(id)) return i;
+        }
+        return Constant.NOT_FOUND;
+    }
+
+    public Grade getGradeById(String id){
+        int index = getGradeIndex(id);
+        return index == Constant.NOT_FOUND ? new Grade() : getGrade(index);
+    }
+
+    public void submitGrade(Grade grade, RedirectAttributes  redirectAttributes){
+        
+        int index = getGradeIndex(grade.getId());
+        if (index == Constant.NOT_FOUND){
+            redirectAttributes.addFlashAttribute("status", Constant.SUCCESS_STATUS);
+            addGrade(grade);
+        }else{
+            redirectAttributes.addFlashAttribute("status", Constant.UPDATE_STATUS);
+            updateGrade(grade, index);
+        }
+    }
+}
---
+++ Challenge_Workbook_Spring/DEPENDENCY-INJECTION/Challenge-Depend-Injec/src/test/java/com/opeyemi/fieldvalidation/FieldValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opeyemi.fieldvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FieldValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
