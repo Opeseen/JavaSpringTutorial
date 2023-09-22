be32071a71eedd98ce107f16088019e8a1db8603
Opeseen <Opeyemi5055@yahoo.com>
2023-09-22 20:07:27 +0100

    Deployed

--- LectureSpring/contacts-rest-api/starter/contacts/src/main/java/com/ltp/contacts/repository/ContactRepository.java
+++ LectureSpring/contacts-rest-api/starter/contacts/src/main/java/com/ltp/contacts/repository/ContactRepository.java
@@ -1,6 +1,6 @@
 package com.ltp.contacts.repository;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
@@ -10,7 +10,11 @@ import com.ltp.contacts.pojo.Contact;
 @Repository
 public class ContactRepository {
     
-    private List<Contact> contacts = new ArrayList<>();
+    private List<Contact> contacts = Arrays.asList(
+        new Contact("123","Mike John","8072654320"),
+        new Contact("132","Opeyemi Alabi","8077563920"),
+        new Contact("123","Yomex Joe","90724514320")
+    );
 
     public List<Contact> getContacts() {
         return contacts;
