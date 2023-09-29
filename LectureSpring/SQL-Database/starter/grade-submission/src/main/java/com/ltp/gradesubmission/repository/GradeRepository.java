9b5fcd9c734f722c6f2aa4e9b5723fae4f759f0a
Opeseen <Opeyemi5055@yahoo.com>
2023-09-29 19:55:05 +0100

    Deployed

--- LectureSpring/SQL-Database/starter/grade-submission/src/main/java/com/ltp/gradesubmission/repository/GradeRepository.java
+++ LectureSpring/SQL-Database/starter/grade-submission/src/main/java/com/ltp/gradesubmission/repository/GradeRepository.java
@@ -1,1 +1,11 @@
 package com.ltp.gradesubmission.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ltp.gradesubmission.entity.Grade;
+
+public interface GradeRepository extends CrudRepository<Grade, Long>{
+    List<Grade> findByStudentId(Long StudentId);
+}
