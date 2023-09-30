ea3c4a0c3d7a160d1b5b15987c554a736b90f474
Opeseen <Opeyemi5055@yahoo.com>
2023-09-30 10:02:29 +0100

    Deployed

---
+++ Self-Practice/grade-submission/src/main/java/com/opeyemi/gradesubmission/GradeController.java
@@ -0,0 +1,68 @@
+package com.opeyemi.gradesubmission;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Controller
+public class GradeController {
+  List<Grade> studentGrades = new ArrayList<>();
+  
+  @GetMapping("/conditionals")
+  public String getMethodName(Model model){
+    model.addAttribute("sale",150);
+    model.addAttribute("sales",50);
+    model.addAttribute("product", "table");
+    return "conditionals";
+  }
+
+  @PostMapping("/handleSubmit")
+  @ResponseStatus(code = HttpStatus.OK)
+  public String SubmitForm(Grade grade){
+    int index = getGradeIndex(grade.getId());
+    if (index == Constant.NOT_FOUND) studentGrades.add(grade);
+    else studentGrades.set(index,grade);
+    return "redirect:/grade";
+  }
+
+  @GetMapping("/grade")
+  @ResponseStatus(code = HttpStatus.OK)
+  public String getGrades(Model model){
+    model.addAttribute("grade", studentGrades);
+    return "grades";
+  }
+
+  public int getGradeIndex(String id){
+    for(int i = 0; i < studentGrades.size(); i++){
+      if(studentGrades.get(i).getId().equals(id)) return i;    
+    }
+    return Constant.NOT_FOUND;
+  }
+
+  @GetMapping(value = "/view")
+  public String getMethodValue(Model model){
+    model.addAttribute("menu", "we sell chocolate rice cakes bubble tea");
+    return "view";
+  }
+
+  @GetMapping(value = "/")
+  // @ResponseStatus(code = HttpStatus.OK)
+  public String gradeForm(Model model, @RequestParam(required = false) String id){
+    int index = getGradeIndex(id);
+    model.addAttribute("grade", index == Constant.NOT_FOUND ? new Grade() : studentGrades.get(index));
+    
+    return "form";
+  }
+
+}
