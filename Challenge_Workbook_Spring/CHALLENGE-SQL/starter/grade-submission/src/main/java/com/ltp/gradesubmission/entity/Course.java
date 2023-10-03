f26c381ae174a52fc685937964b73e5b10719fb0
Opeseen <Opeyemi5055@yahoo.com>
2023-10-03 19:48:55 +0100

    Deployed

--- Challenge_Workbook_Spring/CHALLENGE-SQL/starter/grade-submission/src/main/java/com/ltp/gradesubmission/entity/Course.java
+++ Challenge_Workbook_Spring/CHALLENGE-SQL/starter/grade-submission/src/main/java/com/ltp/gradesubmission/entity/Course.java
@@ -8,9 +8,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+
+import org.hibernate.annotations.JoinFormula;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -45,5 +50,13 @@ public class Course {
     @JsonIgnore
     @OneToMany(mappedBy = "course", cascade = CascadeType.ALL)
     private List<Grade> grades;
+
+    @ManyToMany
+    @JoinTable(
+        name = "course_student", 
+        joinColumns = @JoinColumn(name = "course_id", referencedColumnName = "id"),
+        inverseJoinColumns = @JoinColumn(name = "student_id", referencedColumnName = "id")
+    )
+    private List<Student> students;
 
 }
--- Challenge_Workbook_Spring/CHALLENGE-SQL/starter/grade-submission/src/main/java/com/ltp/gradesubmission/entity/Student.java
+++ Challenge_Workbook_Spring/CHALLENGE-SQL/starter/grade-submission/src/main/java/com/ltp/gradesubmission/entity/Student.java
@@ -9,10 +9,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Past;
+
+import org.hibernate.annotations.ManyToAny;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -44,5 +47,9 @@ public class Student {
     @JsonIgnore
     @OneToMany(mappedBy = "student", cascade = CascadeType.ALL)
     private List<Grade> grades;
+
+    @ManyToMany(mappedBy = "students")
+    private List<Course> courses;
+
 
 }
