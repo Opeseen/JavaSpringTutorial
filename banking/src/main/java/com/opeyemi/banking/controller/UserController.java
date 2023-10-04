d60f797c84c946a279dfbc978ebf3fbb4dcdd26b
Opeseen <Opeyemi5055@yahoo.com>
2023-10-04 15:38:20 +0100

    Deployed

--- banking/src/main/java/com/opeyemi/banking/controller/UserController.java
+++ banking/src/main/java/com/opeyemi/banking/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.opeyemi.banking.entity.User;
