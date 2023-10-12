5e58fc5a4d0edc38a0a7569d6a57a546cdd29e40
Opeseen <Opeyemi5055@yahoo.com>
2023-10-12 11:57:57 +0100

    Deployed

--- banking/src/main/java/com/opeyemi/banking/services/EmailServiceImp.java
+++ banking/src/main/java/com/opeyemi/banking/services/EmailServiceImp.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 import com.opeyemi.banking.helpers.EmailSender;
 
 @Service
-public class EmailSvcImp implements EmailServices{
+public class EmailServiceImp implements EmailServices{
 
   @Autowired
   JavaMailSender javaMailSender;
