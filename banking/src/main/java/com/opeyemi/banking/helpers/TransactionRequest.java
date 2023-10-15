7eeb727c3b9aa1976f7a25783035ebbc9f6f54ac
Opeseen <Opeyemi5055@yahoo.com>
2023-10-15 23:49:35 +0100

    Dep;oyed

--- banking/src/main/java/com/opeyemi/banking/helpers/TransactionRequest.java
+++ banking/src/main/java/com/opeyemi/banking/helpers/TransactionRequest.java
@@ -1,4 +1,5 @@
 package com.opeyemi.banking.helpers;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
@@ -12,6 +13,7 @@ import lombok.*;
 public class TransactionRequest {
 
   @NotBlank(message = "Amount cannot be blank")
+  @Min(value = 100, message = "Amount cannot be less than 100")
   private String amount;
 
   private String transType;
