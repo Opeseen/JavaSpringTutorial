db36e2f66eb570d31f259ed13284ec17621fc219
Opeseen <Opeyemi5055@yahoo.com>
2023-11-14 09:51:44 +0100

    Deployed

--- LectureSpring/Spring basic security/starter/contacts/src/main/java/com/ltp/contacts/security/SecurityConfig.java
+++ LectureSpring/Spring basic security/starter/contacts/src/main/java/com/ltp/contacts/security/SecurityConfig.java
@@ -9,6 +9,6 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
-
+    okay
   }
 }
