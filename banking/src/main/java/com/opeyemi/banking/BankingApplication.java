cfd7f5cf6f5eec3364904921f1bc425b8106867f
Horpeyemi <Opeyemi5055@yahoo.com>
2024-02-29 09:40:56 +0100

    Deployed

--- banking/src/main/java/com/opeyemi/banking/BankingApplication.java
+++ banking/src/main/java/com/opeyemi/banking/BankingApplication.java
@@ -2,9 +2,10 @@ package com.opeyemi.banking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BankingApplication {
+public class BankingApplication extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankingApplication.class, args);
