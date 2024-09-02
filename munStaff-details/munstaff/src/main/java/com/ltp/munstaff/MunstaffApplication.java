e92ee20f04b644ee785ab7a6ec8c970a29801603
Horpeyemi <Opeyemi5055@yahoo.com>
2024-09-02 19:07:04 +0100

    Deployed

---
+++ munStaff-details/munstaff/src/main/java/com/ltp/munstaff/MunstaffApplication.java
@@ -0,0 +1,13 @@
+package com.ltp.munstaff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunstaffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MunstaffApplication.class, args);
+	}
+
+}
---
+++ munStaff-details/munstaff/src/test/java/com/ltp/munstaff/MunstaffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltp.munstaff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunstaffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
