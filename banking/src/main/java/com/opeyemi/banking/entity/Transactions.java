3d4c015f27a3733f662be4c5256c0b2b79cf769a
Opeseen <Opeyemi5055@yahoo.com>
2024-09-19 22:09:39 +0100

    Deployed

--- banking/src/main/java/com/opeyemi/banking/entity/Transactions.java
+++ banking/src/main/java/com/opeyemi/banking/entity/Transactions.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.format.annotation.DateTimeFormat;
 
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -15,7 +14,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.*;
-
 
 @Builder
 @Getter
@@ -45,8 +43,6 @@ public class Transactions {
 
 	@Column(name = "description")
 	private String description;
-
-
 
 	@ManyToOne(optional = false)
 	@JoinColumn(name = "user_id", referencedColumnName = "id")
